d5cee033f538bec60569563bce501e264361a430
Alina Petrenko <alina.petrenko@globallogic.com>
2022-04-21 14:13:41 +0200

    Added AutoResetEvent

---
+++ ThreadsTask/AutoResetEventTask/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+
+namespace AutoResetEventTask
+{
+    /// <summary>
+    /// Represents working with 'AutoResetEvent'
+    /// </summary>
+    public class Program
+    {
+        #region Private Fields
+
+        /// <summary>
+        /// AutoResetEvent object with false value
+        /// </summary>
+        private static AutoResetEvent autoResetEventFalse = new(false);
+
+        /// <summary>
+        /// AutoResetEvent object with true value
+        /// </summary>
+        private static AutoResetEvent autoResetEventTrue = new(true);
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Entry point in project
+        /// </summary>
+        static void Main()
+        {
+            for (var i = 1; i < 6; i++)
+            {
+                var myThread = new Thread(Process);
+                myThread.Name = $"Thread {i}";
+                myThread.Start();
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Prints the value of a variable and the thread that modified the variable
+        /// </summary>
+        public static void Process()
+        {
+            var value = 1;
+            Console.WriteLine($"{Thread.CurrentThread.Name} waits on autoResetEventTrue");
+            autoResetEventTrue.WaitOne();
+            for (var i = 1; i < 6; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} with true: {value}");
+                value++;
+                Thread.Sleep(100);
+            }
+            autoResetEventTrue.Set();
+            Thread.Sleep(3000);
+
+            value = 1;
+            Console.WriteLine($"{Thread.CurrentThread.Name} waits on autoResetEventFalse");
+            autoResetEventFalse.Set();
+            autoResetEventFalse.WaitOne();
+            for (var i = 1; i < 6; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} with false: {value}");
+                value++;
+                Thread.Sleep(100);
+            }
+            autoResetEventFalse.Set();
+            Thread.Sleep(3000);
+        }
+
+        #endregion
+    }
+}
