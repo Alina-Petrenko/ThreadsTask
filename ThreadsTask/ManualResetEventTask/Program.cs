061a358a6b485be8d2e4ff07f4f258e0a8d03a9a
Alina Petrenko <alina.petrenko@globallogic.com>
2022-04-21 14:32:20 +0200

    Added ManualResetEvent

---
+++ ThreadsTask/ManualResetEventTask/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+
+namespace ManualResetEventTask
+{
+    /// <summary>
+    /// Represents working with 'ManualResetEvent'
+    /// </summary>
+    public class Program
+    {
+        #region Private Fields
+
+        /// <summary>
+        /// ManualResetEvent object with false value
+        /// </summary>
+        private static ManualResetEvent manualResetEvent = new(false);
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Entry point in project
+        /// </summary>
+        static void Main()
+        {
+            for (var i = 1; i < 5; i++)
+            {
+                var myThread = new Thread(ThreadInfoOutput);
+                myThread.Name = $"Thread {i}";
+                myThread.Start();
+            }
+            Thread.Sleep(1000);
+            manualResetEvent.Set();
+            Thread.Sleep(1000);
+
+            for (var i = 5; i < 10; i++)
+            {
+                var myThread = new Thread(ThreadInfoOutput);
+                myThread.Name = $"Thread {i}";
+                myThread.Start();
+            }
+            Thread.Sleep(1000);
+            manualResetEvent.Reset();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Outputs info about Thread
+        /// </summary>
+        public static void ThreadInfoOutput()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} starts and calls WaitOne()");
+            manualResetEvent.WaitOne();
+            Console.WriteLine($"{Thread.CurrentThread.Name} ends");
+        }
+
+        #endregion
+    }
+}
