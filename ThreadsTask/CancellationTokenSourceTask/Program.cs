a353369dfac190cfa7a06dffa44e72af8fb4534e
Alina Petrenko <alina.petrenko@globallogic.com>
2022-04-21 16:49:20 +0200

    Added CancellationTokenSource

---
+++ ThreadsTask/CancellationTokenSourceTask/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CancellationTokenSourceTask
+{
+    /// <summary>
+    /// Represents working with 'CountdownEvent'
+    /// </summary>
+    public class Program
+    {
+        #region Private Methods
+
+        /// <summary>
+        /// Entry point in project
+        /// </summary>
+        static void Main()
+        {
+            CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancelTokenSource.Token;
+
+            Task task = new Task(() =>
+            {
+                for (var i = 1; i < 10; i++)
+                {
+                    if (token.IsCancellationRequested)
+                    {
+                        Console.WriteLine("Operation canceled");
+                        return;
+                    }
+                    Console.WriteLine($"Value: {i}");
+                    Thread.Sleep(300);
+                }
+            }, token);
+            task.Start();
+            Thread.Sleep(2000);
+            cancelTokenSource.Cancel();
+            Thread.Sleep(1000);
+            Console.WriteLine($"Task Status: {task.Status}");
+            cancelTokenSource.Dispose();
+        }
+
+        #endregion
+    }
+}
