5ba345a37cd2fa198db67b49bc03d8284374b254
Alina Petrenko <alina.petrenko@globallogic.com>
2022-04-21 14:13:23 +0200

    Added SemaphoreSlim

---
+++ ThreadsTask/SemaphoreSlimTask/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace SemaphoreSlimTask
+{
+    /// <summary>
+    /// Represents working with 'SemaphoreSlim'
+    /// </summary>
+    public class Program
+    {
+        #region Private Methods
+
+        /// <summary>
+        /// Entry point in project
+        /// </summary>
+        static void Main()
+        {
+            var semaphore = new SemaphoreSlim(2);
+            for (var i = 1; i < 6; i++)
+            {
+                new Thread((value) =>
+                {
+                    Console.WriteLine($"Thread {value} wait");
+                    semaphore.Wait();
+                    Console.WriteLine($"Thread {value} in");
+                    Thread.Sleep(2000);
+                    Console.WriteLine($"Thread {value} out");
+                    semaphore.Release();
+                }
+                    ).Start(i);
+            }
+        }
+
+        #endregion
+    }
+}
