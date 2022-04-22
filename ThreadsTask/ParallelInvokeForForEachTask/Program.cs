67a466298bbcfa23df10c5ede616005a476fcd5f
Alina Petrenko <alina.petrenko@globallogic.com>
2022-04-22 14:36:40 +0200

    Added Parallel Task

---
+++ ThreadsTask/ParallelInvokeForForEachTask/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParallelTask
+{
+    /// <summary>
+    /// Represents working with 'Parallel'
+    /// </summary>
+    public class Program
+    {
+        #region Private Methods
+
+        /// <summary>
+        /// Entry point in project
+        /// </summary>
+        static void Main()
+        {
+            var rand = new Random();
+            var array = new int[50];
+            Parallel.For(0, array.Length, i => array[i] = rand.Next(0, 1001));
+            Parallel.ForEach(array, value => Console.Write($"{value} "));
+            Parallel.Invoke(
+            () =>
+                Console.WriteLine($"\nTask {Task.CurrentId} is processed."),
+            () => Print(),
+            () => Find(array)
+            );
+
+        }
+
+        /// <summary>
+        /// Prints Info about current task
+        /// </summary>
+        private static void Print()
+        {
+            Console.WriteLine($"Task {Task.CurrentId} is processed.");
+        }
+
+        /// <summary>
+        /// Finds the biggest value in <paramref name="array"/>
+        /// </summary>
+        /// <param name="array">Array</param>
+        private static void Find(int[] array)
+        {
+            Console.WriteLine($"Task {Task.CurrentId} is processed.");
+            var value = array.Max(x => x);
+            Console.WriteLine($"The biggest value is {value}");
+        }
+
+        #endregion
+    }
+}
