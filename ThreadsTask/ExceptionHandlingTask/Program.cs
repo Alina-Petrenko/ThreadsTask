02d2e0d0fb6d8c897e5c9240a427e98285a034d2
Alina Petrenko <alina.petrenko@globallogic.com>
2022-04-22 16:47:12 +0200

    Added ExceptionHandlingTask

---
+++ ThreadsTask/ExceptionHandlingTask/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingTask
+{
+    /// <summary>
+    /// Represents working with 'Exception-Handling'
+    /// </summary>
+    public class Program
+    {
+        #region Private Methods
+
+        /// <summary>
+        /// Entry point to project
+        /// </summary>
+        private static void Main()
+        {
+            var rand = new Random();
+            Task<int> task = Task.Factory.StartNew(() => rand.Next(1,1001) / 0);
+            try
+            {
+                Console.WriteLine($"Result: {task.Result}");
+            }
+            catch (AggregateException exception)
+            {
+                Console.Write(exception.InnerException.Message);
+            }
+        }
+
+        #endregion
+    }
+}
